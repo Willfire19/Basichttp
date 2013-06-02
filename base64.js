55fe62ff10894a2e7e4f5163b82dab68d0b5c7fe
Willfire19 <willfire19@gmail.com>
2013-06-02 13:36:55 -0400

    Decode input box has been added. Jquery code has been refactored some.

--- base64.js
+++ base64.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 	var error = $('<p>Username and Password must be at least 3 characters long</p>');
-	$(':submit').click(function(e) {
+	/*$(':submit').click(function(e) { */
+		$('#encode').click(function(e) {
 		$(':text').each(function() {
-			if ($(this).val().length < 3) {
+			var username = $("#username").val();
+			var password = $("#password").val();
+			if (username.length < 3 || password.length < 3) {
 				$("form").append(error);
 			}
 			else{
-				var username = $("#username").val();
-				var password = $("#password").val();
+				
 				var encode_string = username+':'+password;
 				var base64_encode = btoa(encode_string);
 				$("p4").text('Authorization: Basic '+base64_encode);
