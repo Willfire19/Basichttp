b83eb9f843c093a1d84ed64da8c09399cfe7c4e1
Willfire19 <willfire19@gmail.com>
2013-06-04 10:49:14 -0400

    The decoded string now needs the Basic word in order to work. Error messages have also been fixed. Encoding a username and password will now include the Basic word in the answer.

--- base64.js
+++ base64.js
@@ -1,24 +1,20 @@
 $(document).ready(function(){
-	var error = $('<p>Username and Password must be at least 3 characters long</p>');
-	/*$(':submit').click(function(e) { */
 		$('#encode').click(function(e) {
-		$(':text').each(function() {
 			var username = $("#username").val();
 			var password = $("#password").val();
 			if (username.length < 3 || password.length < 3) {
-				$("form").append(error);
+				$("p").text("Username and/or password needs to be at least 3 characters long");
 			}
 			else{
-				
+				$("p").text("");
 				var encode_string = username+':'+password;
 				var base64_encode = btoa(encode_string);
 				$("p4").text('Authorization: Basic '+base64_encode);
 				$("p5").text('The string that will be encoded is: '+encode_string);
 				$("p1").text('The Username is: '+username);
 				$("p2").text('The Password is: '+password);
-				$("#decode").val(base64_encode);
+				$("#decode").val("Basic"+base64_encode);
 			}
-		});
 		e.preventDefault();
 	});
 });
--- decode_base64.js
+++ decode_base64.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
-	var error = $('<p>You may want to put a string in the Decode Base64</p>');
-	/*$(':submit').click(function(e) { */
 		$('#decode_button').click(function(e) {
-		$(':text').each(function() {
 			var decode = $("#decode").val();
 			if (decode.length == 0) {
-				$("form").append(error);
+				$("p").text("You may want to put a string in the Decode Base64");
+			}
+			var correct_decode = decode.split(/ +/);
+			$("p1").text('The Base64 string is: '+correct_decode[0]+' '+correct_decode[1]);
+			if (correct_decode[0] != "Basic") {
+				$("p").text("The word 'Basic' is needed in the decode input");
 			}
 			else{
-				var decoded_string = atob(decode);
+				$("p").text("");
+				var decoded_string = atob(correct_decode[1]);
 				$("p5").text('The decoded string is: '+decoded_string);
 				var decoded_split = decoded_string.split(':');
 				var username = decoded_split[0];
@@ -18,7 +21,6 @@ $(document).ready(function(){
 				$("#username").val(username);
 				$("#password").val(password);
 			}
-		});
 		e.preventDefault();
 	});
 });
